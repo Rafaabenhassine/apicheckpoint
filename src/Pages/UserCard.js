f83680e4cf3d32030f28be3851df9ee5609c968f
Rafaabenhassine <rafaabenhassine1@gmail.com>
2023-09-21 10:58:00 +0100

    update

--- src/Pages/UserCard.js
+++ src/Pages/UserCard.js
@@ -3,26 +3,29 @@ import React from 'react'
 const UserCard = () => {
   return (
     [
-        <h4>Name:{user.name}</h4>,
-        <h4>Username:{user.username}</h4>,
-        <h4>Email:{user.email}</h4>,
-        <h4>Adress:{user.adress}</h4>,
-        <h4>Street:{user.street}</h4>,
-        <h4>Suite:{user.suite}</h4>,
-        <h4>City:{user.city}</h4>,
-        <h4>Zipcode:{user.zipcode}</h4>,
-        <h4>Geo:{user.geo}</h4>,
-        <h4>Lat:{user.lat}</h4>,
-        <h4>Lng:{user.lng}</h4>,
-        <h4>Phone:{user.phone}</h4>,
-        <h4>Website:{user.website}</h4>,
-        <h4>Company:{user.company}</h4>,
-        <h4>Name:{user.name}</h4>,
-        <h4>CatchPhrase:{user.catchPhrase}</h4>,
-        <h4>Bs:{user.bs}</h4>
+      {
+        id:1,
+        Name:"user.name",
+        Username:"user.username",
+        Email:"user.email",
+        Adress:"user.adress",
+        Street:"user.street",
+        Suite:"user.suite",
+        City:"user.city",
+        Zipcode:"user.zipcode",
+        Geo:"user.geo",
+        Lat:"user.lat",
+        Lng:"user.lng",
+        Phone:"user.phone",
+        Website:"user.website",
+        Company:"user.company",
+        Name:"user.name",
+        CatchPhrase:"user.catchPhrase",
+        Bs:"user.bs"
+      }
     ]
   )
-}
+  }
 
 
 export default UserCard
